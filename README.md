5bfc0b7bf98ef4312ff791ae6820e2cad7336d20
RezaEnekaas <RezaEnekaas@gmail.com>
2020-11-15 21:24:28 +0330

    update source

---
+++ README.md
@@ -0,0 +1,22 @@
+👋 Im Alireza
+===========
+
+## Welcome To Readme-Generator!
+
+ > Hi my name is Alireza and i developer of Readme-Generator :)
+Repo of This Project: https://github.com/Enekaas/readme-generator
+* # For Run:
+
+                        ```bash
+                            git clone https://github.com/Enekaas/readme-generator
+
+                            cd readme-generator
+
+                            npm start
+                        ```
+
+* Keywords:
+#readme,generate
+
+### - Made With ❤ By Readme-Generator
+    					
---
+++ index.js
@@ -0,0 +1,75 @@
+/*
+ ===================
+ * # Author: Alireza
+ * # myTelegram: @DarkRise
+ * # Repo: github.com/Enekaas
+ ===================
+*/
+
+// # import libs
+const { createInterface } = require("readline");
+const { createWriteStream } = require("fs");
+
+// # create interFace
+const rl = createInterface(process.stdin, process.stdout);
+
+// # create information Object
+var person = {
+  name: "",
+  title: "",
+  repo: "",
+  runCommand: "",
+  keywords: [],
+};
+
+// # ask questions
+rl.question(" 👤 | > Enter Your Name: ", (name) => {
+  let stream = createWriteStream(`./${name}.md`);
+  person.name = name;
+  rl.question(
+    ` ☕ | - Dear(${name}), Please Enter Your App Title: `,
+    (appName) => {
+      person.title = appName;
+      rl.question(
+        ` ✨ | > Please Enter Repository for (${appName}): `,
+        (repo) => {
+          person.repo = repo;
+          rl.question(
+            " 🔸 | - Enter Run Command For Your Project (ex: 'npm start'): ",
+            (run) => {
+              person.runCommand = run;
+              rl.question(
+                " 💫 | > Enter Keywords: ",
+                (keywords) => {
+                  for (let i of keywords.split(" ")) {
+                    person.keywords.push(i);
+                  }
+                  stream.write(`👋 Im ${name}\n===========\n\n## Welcome To ${appName}!\n\n > Hi my name is ${name} and i developer of ${appName} :)\nRepo of This Project: ${repo}\n* # For Run:\n
+                        \`\`\`bash
+                            git clone ${repo}\n
+                            cd ${repo.split('/').splice(-1)}\n
+                            ${run}
+                        \`\`\`\n\n* Keywords:\n#${person.keywords}\n\n### - Made With ❤ By Readme-Generator
+    					`);
+                  stream.close();
+                  rl.close();
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
+});
+
+// # handle close readline
+rl.on("close", () => {
+  console.log(` 🎉 | File Saved With Above Informations:\n${JSON.stringify(person, null, 2)}`);
+});
+
+// # handle Ctrl+C
+process.on("SIGUSR1", () => {
+  console.log(" ❌ | - Unhandled Exit!");
+  process.exit();
+});
